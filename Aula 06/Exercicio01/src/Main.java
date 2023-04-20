2f3daec9db5d2f5f143aa2775c74d9b1cfa83e41
GMalvestiti <gustavo.smalvestiti@gmail.com>
2023-04-19 21:15:32 -0300

    aula 6

--- Aula 06/Exercicio01/src/Main.java
+++ Aula 06/Exercicio01/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Circulo c = new Circulo(2, "Vermelho");
-        System.out.println(c.toString());
+        System.out.println("\n" + c.toString());
 
         Triangulo t = new Triangulo(3, 4, "Verde");
         System.out.println(t.toString());
@@ -11,6 +11,6 @@ public class Main {
         System.out.println(r.toString());
 
         Quadrado q = new Quadrado(7, "Amarelo");
-        System.out.println(q.toString());
+        System.out.println(q.toString() + "\n");
     }
 }
